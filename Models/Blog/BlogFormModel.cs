023fbc6469aae9848cd019bf408f1f7559690aaa
phamsn2001 <phamsn2001@gmail.com>
2021-06-02 13:50:37 +0700

    BlogListModel Update

---
+++ Models/Blog/BlogFormModel.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.InterfaceImpls;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.Models
+{
+    public class BlogFormModel
+    {
+        public Post Post { get; set; }
+        public IFormFileCollection Images { get; set; }
+    }
+}
---
+++ Models/Blog/BlogListModel.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.InterfaceImpls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.Models.Blog
+{
+    public class BlogListModel
+    {
+        public IEnumerable<Post> Posts { get; set; }
+        public bool ShowClosed { get; set; }
+    }
+}
