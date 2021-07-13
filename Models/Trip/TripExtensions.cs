fb0742113e715063ed7452dc8f2294bf4d7c2ee0
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-07-13 22:03:51 +0700

    Trip Now Requires StartTime to be 31 days ahead

--- Models/Trip/TripExtensions.cs
+++ Models/Trip/TripExtensions.cs
@@ -10,22 +10,22 @@ namespace Company.Models
         {
             Dictionary<string, List<string>> errors = new Dictionary<string, List<string>>();
 
-            if (trip.StartTime >= trip.EndTime)
-            {
-                if (errors.GetValueOrDefault("Trip.StartTime") == null)
-                {
-                    errors["Trip.StartTime"] = new List<string>();
-                }
-                errors["Trip.StartTime"].Add("Start time cannot be after end time");
-            }
-
-            if (trip.EndTime <= DateTime.Now)
+            if (trip.EndTime <= trip.StartTime)
             {
                 if (errors.GetValueOrDefault("Trip.EndTime") == null)
                 {
                     errors["Trip.EndTime"] = new List<string>();
                 }
-                errors["Trip.EndTime"].Add("End time cannot be in the past");
+                errors["Trip.EndTime"].Add("End time cannot be before start time");
+            }
+
+            if (trip.StartTime < DateTime.Now.AddDays(31))
+            {
+                if (errors.GetValueOrDefault("Trip.StartTime") == null)
+                {
+                    errors["Trip.StartTime"] = new List<string>();
+                }
+                errors["Trip.StartTime"].Add("Start time must be at least 31 days from now");
             }
             return errors;
         }
